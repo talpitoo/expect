420aed7529396ba7a385ca8dc0f09979c3677535
Tamás Tóth <mounttain@gmail.com>
2016-01-24 12:45:30 +0100

    updated G+ icon, added ASCII art

--- js/main.js
+++ js/main.js
@@ -87,9 +87,9 @@ $(function () {
 // replace low quality images after the page loads
 $(window).load(function () {
     if ($(window).width() >= 768) {
-        var $headerImage = $('<img src="img/stanley-kubrick-2001-space-odyssey.jpg?ver=13012016">');
+        var $headerImage = $('<img src="img/stanley-kubrick-2001-space-odyssey.jpg?ver=24012016">');
         $headerImage.bind('load', function () {
-            $('header').css('background-image', 'linear-gradient(to right, rgba(0, 0, 0, 0.5), transparent), url(img/stanley-kubrick-2001-space-odyssey.jpg?ver=13012016)');
+            $('header').css('background-image', 'linear-gradient(to right, rgba(0, 0, 0, 0.5), transparent), url(img/stanley-kubrick-2001-space-odyssey.jpg?ver=24012016)');
         });
     }
 });
